e6677039752b1b486df06cb4d75452b9741ff032
Christopher Millson <christopher.r.millson.mil@swf.army.mil>
2022-11-08 16:03:22 -0600

    Basic Front End Skeleton Complete with Cat Tax

---
+++ src/LandingPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import ButtonAppBar from "./Components/AppBar";
+import Box from "@mui/material/Box";
+import {List, Stack} from "@mui/material";
+
+export default function LandingPage() {
+
+    return(
+        <Box justifyContent={'center'} display={'flex'}>
+           <img  src={"/cat.gif"} />
+        </Box>
+
+    )
+}
---
+++ src/SupervisorView.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import ButtonAppBar from "./Components/AppBar";
+import Box from "@mui/material/Box";
+import {List, Stack} from "@mui/material";
+
+export default function SupervisorView() {
+
+    return(
+        <Box>
+            <ul>
+                <li>List of Reports</li>
+                <li>Lisrt of Reports</li>
+            </ul>
+
+        </Box>
+
+    )
+}
