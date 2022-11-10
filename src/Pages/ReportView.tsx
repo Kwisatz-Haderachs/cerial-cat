9a5d992e0ed667a9f37e2395629182af38390a8c
Kwisatz-Haderachs <frcranmer@gmail.com>
2022-11-10 10:35:48 -0600

    Data validation with most fields, multiselect chip rendering not labled

--- src/Pages/ReportView.tsx
+++ src/Pages/ReportView.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {TextInput, Select} from '@mantine/core';
+import {TextInput, Select, Checkbox, MultiSelect} from '@mantine/core';
 import { DatePicker, TimeInput } from '@mantine/dates';
 import { useForm } from '@mantine/form';
 import {Grid, Stack, Box} from "@mui/material";
@@ -7,25 +7,61 @@ import {Grid, Stack, Box} from "@mui/material";
 
 export default function ReportView() {
 
-    // export interface ReportForm{
-    //     dateOfEvent: string,
-    //     timeOfEvent: string,
-    //     locationOfEvent: string,
-    //     eventType: boolean,
-    //     harm: boolean,
-    //     individuals:[],
-    //     typeOfEvent:[],
-    //     effectOfIncident:boolean,
-    //     witness:[],
-    //     witnessNumbers:[],
-    //     departmentsInvolved:[],
-    //     description:string,
-    //     actions:string,
-    //     patientName:string,
-    //     patientPhone:string,
-    //     patientSSN:string,
-    //     patientAddress:string
-    // }
+    const departments = [
+        {value: 'Ambulatory Care'},
+        {value: 'Behavioral/Mental Health'},
+        {value: 'Dental'},
+        {value: 'Emergency Care'},
+        {value: 'Information Management'},
+        {value: 'Laboratory'},
+        {value: 'Logistics'},
+        {value: 'Medicine'},
+        {value: 'Nursing'},
+        {value: 'OB/GYN'},
+        {value: 'Pediatrics'},
+        {value: 'Pharmacy'},
+        {value: 'Radiology'},
+        {value: 'Surgery'},
+        {value: 'Other'}
+    ];
+
+    // const individualsInvolved = [
+    //     {value: 'Patient'},
+    //     {value: 'Family Member - Adult'},
+    //     {value: 'Family Member - Child'},
+    //     {value: 'Staff Member'},
+    //     {value: 'Visitor'},
+    //     {value: 'Volunteer'},
+    //     {value: 'Other'}
+    // ];
+
+    const eventsType = [
+        {value: 'Adverse Drug Reaction'},
+        {value: 'AMA/ Left without being seen'},
+        {value: 'Assault'},
+        {value: 'Blood Products Related'},
+        {value: 'Delay in Diagnosis / Treatment / Transfer'},
+        {value: 'Equipment/Supply Problem'},
+        {value: 'Exposure to Blood / Bodily Fluids'},
+        {value: 'Facility /  Physical Plant Problem'},
+        {value: 'Fall'},
+        {value: 'Infant Abduction'},
+        {value: 'Infant Discharged to Wrong Family'},
+        {value: 'Laboratory Related'},
+        {value: 'Medication Related'},
+        {value: 'Needle Stick / Sharp Injury'},
+        {value: 'Obstetrics Related'},
+        {value: 'Operative / Invasive Procedure Related'},
+        {value: 'Property Damaged / Destroyed'},
+        {value: 'Property Lost / Stolen'},
+        {value: 'Radiology Related'},
+        {value: 'Rape'},
+        {value: 'Restrained Patient Injury'},
+        {value: 'Suicide in a 24 Hour Facility'},
+        {value: 'Other'}
+    ];
+
+
 
     const report = useForm({
         initialValues: {
@@ -47,6 +83,20 @@ export default function ReportView() {
             patientSSN: "",
             patientAddress: "",
         },
+
+        validate: {
+            dateOfEvent: (value) => (/^\d\d\d\d-(0?[1-9]|1[0-2])-(0?[1-9]|[12][0-9]|3[01]) (00|[0-9]|1[0-9]|2[0-3]):([0-9]|[0-5][0-9]):([0-9]|[0-5][0-9])$/.test(value) ? null : "Invalid date"),
+            timeOfEvent: (value) =>  (/^(00|[0-9]|1[0-9]|2[0-3]):([0-9]|[0-5][0-9]):([0-9]|[0-5][0-9])$/.test(value) ? null : "Invalid Time" ),
+            locationOfEvent: (value) => (value.length < 8 ? 'Location input is too short' : null),
+            witness: (value) => (value.length < 4 ? 'Please enter first and last name' : null),
+            description:(value) => (value.length < 15 ? 'Enter a more detailed description' : null),
+            actions:(value) => (value.length < 15 ? 'Enter more detailed action' : null),
+            patientName:(value) => (value.length < 3 ? 'Enter full name' : null),
+            patientPhone:(value) => (/^1?-?\(?[0-9]{3}[\-\)][0-9]{3}-[0-9]{4}$/.test(value) ? null : "Invalid Number" ),
+            patientSSN:(value) => (/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/.test(value) ? null : "Invalid SSN" ),
+            patientAddress: (value) => (value.length < 10 ? 'Enter full Address' : null),
+        }
+
     });
 
     return (
@@ -103,12 +153,41 @@ export default function ReportView() {
                         />
                     </Grid>
                 </Grid>
+                <Grid container spacing={2}>
+                    <Grid item xs={6}>
+                        <Checkbox label="Patient" onChange={() =>  report.insertListItem('individuals', "Patient")}/>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Checkbox label="Staff Member" onChange={() =>  report.insertListItem('individuals', "Staff Member")}/>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Checkbox label="Family Member - Adult" onChange={() =>  report.insertListItem('individuals', "Family Member - Adult")}/>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Checkbox label="Visitor" onChange={() =>  report.insertListItem('individuals', "Visitor")}/>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Checkbox label="Family Member - Child" onChange={() =>  report.insertListItem('individuals', "Family Member - Child")}/>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Checkbox label="Volunteer" onChange={() =>  report.insertListItem('individuals', "Volunteer")}/>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Checkbox label="Other" onChange={() =>  report.insertListItem('individuals', "Other")}/>
+                    </Grid>
+                </Grid>
+                <Box>
+                    <MultiSelect
+                        data={eventsType}
+                        placeholder="Select Type of Event"
+                        label="Type of Event"
+                    />
+                </Box>
                 <Box>
                     <Select
                         withAsterisk
                         label="Effect of Incident"
                         data={[
-                            // Input values
                             {value: 'Harm sustained', label: "Harm"},
                             {value: 'Potential Harm', label: "Potential Harm"}
                         ]}
@@ -148,7 +227,6 @@ export default function ReportView() {
                     </Grid>
                     <Grid item xs={6}>
                         <TextInput
-
                             label='Witness'
                             onChange={(event ) => report.insertListItem('witness', event.target.value)}
                         >
@@ -163,12 +241,11 @@ export default function ReportView() {
                     </Grid>
                 </Grid>
                 <Box>
-                    <TextInput
-                        withAsterisk
-                        label='Departments Involved'
-                        onChange={(event ) => report.insertListItem('departmentsInvolved', event.target.value)}
-                    >
-                    </TextInput>
+                    <MultiSelect
+                        data={departments}
+                        placeholder="Select Departments"
+                        label="Departments Involved"
+                        />
                 </Box>
                 <Box>
                     <TextInput
@@ -224,7 +301,7 @@ export default function ReportView() {
                     </TextInput>
                 </Box>
                 <Box>
-
+                    <h2> Submit </h2>
                 </Box>
             </Stack>
         </Box>
