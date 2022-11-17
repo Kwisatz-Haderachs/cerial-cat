7fdbe5e9f9351f4711c33c355945b2d013e64b12
Kwisatz-Haderachs <frcranmer@gmail.com>
2022-11-17 10:09:33 -0600

    DeleteMapping complete

--- src/Components/ListReport.tsx
+++ src/Components/ListReport.tsx
@@ -1,11 +1,14 @@
-import {Table, Button } from "@mantine/core";
-import IconButton from "@mui/material/IconButton";
+import {Table, Button, Alert} from "@mantine/core";
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import axios from "axios";
 
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever';;
 
 
 
 export default function ListReport (props: any) {
+
+
+
 
     const rows = props.reportList.map((element:any) => (
         <tr key={element.id}>
@@ -15,7 +18,7 @@ export default function ListReport (props: any) {
             <td>{element.harm ?  "Harm" : "Potential Harm"}</td>
             <td>{element.individualsInvolved.map((data: string) => (data+", "))}</td>
             <td>{element.eventCategory.map((data: string) => (data+", "))}</td>
-            <td><DeleteForeverIcon /></td>
+            <td><DeleteForeverIcon onClick={() => {props.deleteItem(element.id)}} /></td>
             <td><Button variant={"subtle"} onClick={() => {props.setItemView(element)}}> View </Button></td>
         </tr>
     ));
