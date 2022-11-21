53f494fffeee2f890113317a79fb698d5541c51b
Kwisatz-Haderachs <frcranmer@gmail.com>
2022-11-21 10:13:34 -0600

    work in progress on datagrid

--- src/Components/ListReport.tsx
+++ src/Components/ListReport.tsx
@@ -1,11 +1,16 @@
 import {Table, Button} from "@mantine/core";
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
-
+import {DataGrid} from '@mui/x-data-grid';
 
 export default function ListReport (props: any) {
     // Convert to MUI datagrid - https://mui.com/x/react-data-grid/
     // Notification for successful delete
     // Send to Command
+
+    const columns =[
+        {field: }
+    ]
+
 
     const rows = props.reportList.map((element:any) => (
         <tr key={element.id}>
@@ -21,21 +26,34 @@ export default function ListReport (props: any) {
     ));
 
     return (
-        <Table>
-                <thead>
-                <tr>
-                    <th>Event Date</th>
-                    <th>Location</th>
-                    <th>Incident Type</th>
-                    <th>Harm</th>
-                    <th>Individuals(s)</th>
-                    <th>Event Type</th>
-                    <th>Delete</th>
-                    <th>Deeeets</th>
-                </tr>
-                </thead>
-                    <tbody>{rows}</tbody>
-        </Table>
+
+        <div style={{ height: 400, width: '100%' }}>
+            <DataGrid
+                rows={rows}
+                columns={columns}
+                pageSize={5}
+                rowsPerPageOptions={[5]}
+                checkboxSelection
+            />
+        </div>
+
+
+        //
+        // <Table>
+        //         <thead>
+        //         <tr>
+        //             <th>Event Date</th>
+        //             <th>Location</th>
+        //             <th>Incident Type</th>
+        //             <th>Harm</th>
+        //             <th>Individuals(s)</th>
+        //             <th>Event Type</th>
+        //             <th>Delete</th>
+        //             <th>Deeeets</th>
+        //         </tr>
+        //         </thead>
+        //             <tbody>{rows}</tbody>
+        // </Table>
 
 )}
 /*
